019077c3d52d64f60adc7b9916380af804a764af
Krishna Dubey <krishnadubey1997@gmail.com>
2020-08-23 21:43:40 +0530

    Update backend.tf

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "20904-terraform-state"
+    bucket         = "terraform-state"
     key            = "tf-state/terraform.tfstate"
     region         = "us-east-1"
   }
